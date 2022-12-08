9b1b2e82e0426136020f5112d12c18051d9b7c30
Anthony <anthony.onwuli16@imperial.ac.uk>
2022-12-08 17:56:42 +0000

    removed the old AtomicEmbeddings.py file

--- AtomicEmbeddings/__init__.py
+++ AtomicEmbeddings/__init__.py
@@ -5,4 +5,4 @@ Modules exported by this package:
 - `AtomicEmbeddings`: Provides the `Atomic_Embeddings` class which handles elemental representations.
 """
 
-from AtomicEmbeddings.AtomicEmbeddings import Atomic_Embeddings
+# from AtomicEmbeddings.AtomicEmbeddings import Atomic_Embeddings
