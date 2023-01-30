962f79a4c4936f322a3fe5fed23a4789f261283d
Anthony <anthony.onwuli16@imperial.ac.uk>
2023-01-30 22:41:47 +0000

    Added docstrings to utils/io.py

--- AtomicEmbeddings/utils/io.py
+++ AtomicEmbeddings/utils/io.py
@@ -1,4 +1,4 @@
-# Utility functions for file I/O
+"""IO utils for AtomicEmbeddings."""
 
 import json
 
@@ -6,7 +6,10 @@ import numpy as np
 
 
 class NumpyEncoder(json.JSONEncoder):
+    """Special json encoder for numpy types."""
+
     def default(self, obj):
+        """Encode numpy types."""
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
