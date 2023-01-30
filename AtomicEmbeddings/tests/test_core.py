eddc26e31468e89c324873352736c1e7f8f61eb1
Anthony <anthony.onwuli16@imperial.ac.uk>
2023-01-30 22:41:24 +0000

    Added docstrings to test_core.py

--- AtomicEmbeddings/tests/test_core.py
+++ AtomicEmbeddings/tests/test_core.py
@@ -1,22 +1,23 @@
+"""Test the core module of AtomicEmbeddings."""
 import unittest
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from matplotlib.axes import Axes
 from scipy.stats._result_classes import PearsonRResult
 
-from AtomicEmbeddings import composition
 from AtomicEmbeddings.core import Embedding
 
 
-class TestSequenceFunctions(unittest.TestCase):
+class EmbeddingTest(unittest.TestCase):
+    """Test the Embedding class."""
+
     # High Level functions
 
     def test_Embedding_loading(self):
-        # Test that the skipatom and megnet16 can be loaded
-        skipatom = composition.Embedding.load_data("skipatom")
-        megnet16 = composition.Embedding.load_data("megnet16")
+        """Test that the Embedding class can load the data."""
+        skipatom = Embedding.load_data("skipatom")
+        megnet16 = Embedding.load_data("megnet16")
         assert skipatom.dim == 200
         assert skipatom.embedding_name == "skipatom"
         assert megnet16.dim == 16
@@ -25,6 +26,7 @@ class TestSequenceFunctions(unittest.TestCase):
         assert isinstance(megnet16.citation(), list)
 
     def test_Embeddings_class_magpie(self):
+        """Test that the Embedding class can load the magpie data."""
         magpie = Embedding.load_data("magpie")
         # Check if the embeddings attribute is a dict
         assert isinstance(magpie.embeddings, dict)
@@ -248,7 +250,7 @@ class TestSequenceFunctions(unittest.TestCase):
         assert isinstance(magpie.citation(), list)
         assert isinstance(magpie.citation()[0], str)
         assert magpie._is_el_in_embedding("H")
-        assert isinstance(magpie.create_correlation_df(), pd.DataFrame)
+        assert isinstance(magpie.correlation_df(), pd.DataFrame)
 
         # TO-DO
         # Create tests for checking dataframes and plotting functions
@@ -266,71 +268,20 @@ class TestSequenceFunctions(unittest.TestCase):
             ),
             float,
         )
-        assert isinstance(magpie.create_distance_correlation_df(), pd.DataFrame)
-        assert magpie.create_distance_correlation_df().shape == (
+        assert isinstance(magpie.distance_correlation_df(), pd.DataFrame)
+        assert magpie.distance_correlation_df().shape == (
             len(list(magpie.create_pairs())) * 2 - len(magpie.embeddings),
             5,
         )
-        assert magpie.create_distance_correlation_df().columns.tolist() == [
+        assert magpie.distance_correlation_df().columns.tolist() == [
             "ele_1",
             "ele_2",
             "mend_1",
             "mend_2",
             "euclidean",
         ]
-        assert isinstance(magpie.create_distance_pivot_table(), pd.DataFrame)
+        assert isinstance(magpie.distance_pivot_table(), pd.DataFrame)
         assert isinstance(magpie.plot_distance_correlation(), plt.Axes)
         assert isinstance(
             magpie.plot_distance_correlation(metric="euclidean"), plt.Axes
         )
-
-    # ------------ Compositon.py functions ------------
-    def test_formula_parser(self):
-        LLZO_parsed = composition.formula_parser("Li7La3ZrO12")
-        assert isinstance(LLZO_parsed, dict)
-        assert "Zr" in LLZO_parsed
-        assert LLZO_parsed["Li"] == 7
-
-    def test__get_fractional_composition(self):
-        CsPbI3_frac = composition._get_fractional_composition("CsPbI3")
-        assert isinstance(CsPbI3_frac, dict)
-        assert "Pb" in CsPbI3_frac
-        assert CsPbI3_frac["I"] == 0.6
-
-    def test_Composition_class(self):
-        Fe2O3_magpie = composition.CompositionalEmbedding(
-            formula="Fe2O3", embedding="magpie"
-        )
-        assert isinstance(Fe2O3_magpie.embedding, Embedding)
-        assert Fe2O3_magpie.formula == "Fe2O3"
-        assert Fe2O3_magpie.embedding_name == "magpie"
-        assert isinstance(Fe2O3_magpie.composition, dict)
-        assert {"Fe": 2, "O": 3} == Fe2O3_magpie.composition
-        assert Fe2O3_magpie._natoms == 5
-        assert Fe2O3_magpie.fractional_composition == {"Fe": 0.4, "O": 0.6}
-        assert isinstance(Fe2O3_magpie._mean_feature_vector(), np.ndarray)
-        # Test that the feature vector function works
-        stats = [
-            "mean",
-            "variance",
-            "minpool",
-            "maxpool",
-            "sum",
-            "range",
-            "geometric_mean",
-            "harmonic_mean",
-        ]
-        assert isinstance(Fe2O3_magpie.feature_vector(stats=stats), np.ndarray)
-        assert len(
-            Fe2O3_magpie.feature_vector(stats=stats)
-        ) == Fe2O3_magpie.embedding.dim * len(stats)
-        # Test that the feature vector function works with a single stat
-        assert isinstance(Fe2O3_magpie.feature_vector(stats="mean"), np.ndarray)
-
-    def test_composition_featuriser(self):
-        formulas = ["Fe2O3", "Li7La3ZrO12", "CsPbI3"]
-        formula_df = pd.DataFrame(formulas, columns=["formula"])
-        assert isinstance(composition.composition_featuriser(formula_df), pd.DataFrame)
-        assert composition.composition_featuriser(formula_df).shape == (3, 2)
-        assert isinstance(composition.composition_featuriser(formulas), list)
-        assert len(composition.composition_featuriser(formulas)) == 3
