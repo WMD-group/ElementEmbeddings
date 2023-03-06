c271afc05334919d4b3f94cbb694bf23e4905478
Anthony <anthony.onwuli16@imperial.ac.uk>
2023-03-06 12:17:18 +0000

    updated __init__.py

--- AtomicEmbeddings/__init__.py
+++ AtomicEmbeddings/__init__.py
@@ -4,6 +4,7 @@ Modules exported by this package:
 
 - `AtomicEmbeddings.core`: Provides the `Embedding` class.
 - `AtomicEmbeddings.composition`: Tools to featurise compositions.
+- `AtomicEmbeddings.plotter`: Tools to plot embeddings.
 """
 
 # from AtomicEmbeddings.AtomicEmbeddings import Atomic_Embeddings
