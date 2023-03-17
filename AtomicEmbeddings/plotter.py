eed917f94bc80fe7a46d689ef7daf98fc2a64301
Anthony <anthony.onwuli16@imperial.ac.uk>
2023-03-17 19:44:18 +0000

    heatmap_plotter update

--- AtomicEmbeddings/plotter.py
+++ AtomicEmbeddings/plotter.py
@@ -10,68 +10,82 @@ from .core import Embedding
 def heatmap_plotter(
     embedding: Embedding,
     metric: str,
-    distance: bool = True,
-    correlation: bool = False,
-    figsize: Tuple[int, int] = (36, 24),
-    filename: Optional[str] = None,
-    show: bool = True,
+    sortaxisby: str = "mendeleev",
+    ax: Optional[plt.axes] = None,
+    show_axislabels: bool = True,
     **kwargs,
 ):
     """
-    Plot a heatmap of the embedding.
+    Plot multiple heatmaps of the embeddings.
 
     Parameters
     ----------
     embedding : Embedding
-        The embedding to be plotted.
-    metric : str
-        The distance/correlation metric to be used.
-    distance : bool, optional
-        Whether to plot a distance heatmap, by default True
-    correlation : bool, optional
-        Whether to plot a correlation heatmap, by default False
-    figsize : Tuple[int,int], optional
-        The size of the figure, by default (36, 24)
-    filename : Optional[str], optional
-        The filename to save the figure to, by default None
-    show : bool, optional
-        Whether to show the figure, by default True
+        The embeddings to be plotted.
+    metric : bool, optional
+        Whether to plot a metric distance heatmap, by default False
+    sortaxisby : str, optional
+        The attribute to sort the axis by, by default "mendeleev_number".
+        Options are "mendeleev_number", "atomic_number"
+    ax : Optional[plt.axes], optional
+        The axes to plot on, by default None
+    show_axislabels : bool, optional
+        Whether to show the axis, by default True
     **kwargs
         Additional keyword arguments to pass to seaborn.heatmap
 
     """
-    fig, ax = plt.subplots(figsize=figsize)
-    if correlation:
-        pivot = embedding.pearson_pivot_table()
+    if not ax:
+        fig, ax = plt.subplots()
 
-    elif distance:
-        pivot = embedding.distance_pivot_table(metric=metric)
-    xlabels = [i[1] for i in pivot.index]
-    ylabels = [i[1] for i in pivot.columns]
+    correlation_metrics = ["spearman", "pearson", "cosine_similarity"]
+    distance_metrics = [
+        "euclidean",
+        "manhattan",
+        "cosine_distance",
+        "chebyshev",
+        "wasserstein",
+        "energy",
+    ]
+    if metric in correlation_metrics:
+        p = embedding.pearson_pivot_table()
+
+    elif metric in distance_metrics:
+        p = embedding.distance_pivot_table(metric=metric, sortby=sortaxisby)
+    xlabels = [i[1] for i in p.index]
+    ylabels = [i[1] for i in p.columns]
     sns.heatmap(
-        pivot,
+        p,
         cmap="bwr",
         square="True",
         linecolor="k",
         ax=ax,
-        cbar_kws={"shrink": 0.5},
+        cbar_kws={
+            "shrink": 0.5,
+        },
         xticklabels=True,
         yticklabels=True,
         **kwargs,
     )
-    ax.title.set_text(embedding.embedding_name)
-    ax.set_xticklabels(
-        xlabels,
+    ax.set_title(
+        embedding.embedding_name,
+        fontdict={
+            # "fontsize": 30,
+            "fontweight": "bold"
+        },
     )
-    ax.set_yticklabels(ylabels)
+    if not show_axislabels:
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_xticks([])
+        ax.set_yticks([])
+    else:
+        ax.set_xticklabels(
+            xlabels,
+        )
+        ax.set_yticklabels(ylabels)
     ax.set_xlabel("")
     ax.set_ylabel("")
-
-    fig.tight_layout()
-    if filename:
-        plt.savefig(filename)
-    if show:
-        plt.show()
 
 
 def multi_heatmap_plotter(
