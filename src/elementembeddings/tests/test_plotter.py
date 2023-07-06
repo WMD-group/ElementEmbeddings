e9cc5ed5508e624420b1330973425572ff5b1628
Anthony Onwuli <anthony.onwuli16@imperial.ac.uk>
2023-07-06 21:43:24 +0100

    Added test for plotting euclidean heatmap

--- src/elementembeddings/tests/test_plotter.py
+++ src/elementembeddings/tests/test_plotter.py
@@ -8,22 +8,29 @@ from elementembeddings.core import Embedding
 from elementembeddings.plotter import dimension_plotter, heatmap_plotter
 
 
-class heatmapTest(unittest.TestCase):
+class HeatmapTest(unittest.TestCase):
     """Test the heatmap_plotter function."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up the test class."""
+        cls.test_skipatom = Embedding.load_data("skipatom")
 
     def test_heatmap_plotter(self):
         """Test that the heatmap_plotter function works."""
-        # Load the data
-        skipatom = Embedding.load_data("skipatom")
         # Get the embeddings
         skipatom_cos_plot = heatmap_plotter(
-            skipatom,
+            self.test_skipatom,
             metric="cosine_similarity",
         )
         assert isinstance(skipatom_cos_plot, plt.Axes)
+        skipatom_euc_plot = heatmap_plotter(
+            self.test_skipatom, metric="euclidean", show_axislabels=False
+        )
+        assert isinstance(skipatom_euc_plot, plt.Axes)
 
 
-class dimensionTest(unittest.TestCase):
+class DimensionTest(unittest.TestCase):
     """Test the dimension_plotter function."""
 
     def test_dimension_plotter(self):
