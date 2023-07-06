7f9f87b987a77f1d4b73cf9fed289a5d9a028417
Anthony Onwuli <anthony.onwuli16@imperial.ac.uk>
2023-07-06 09:28:18 +0100

    Added more tests to the Embedding loading function

--- src/elementembeddings/tests/test_core.py
+++ src/elementembeddings/tests/test_core.py
@@ -20,12 +20,17 @@ class EmbeddingTest(unittest.TestCase):
         """Test that the Embedding class can load the data."""
         skipatom = Embedding.load_data("skipatom")
         megnet16 = Embedding.load_data("megnet16")
+        matscholar = Embedding.load_data("matscholar")
         assert skipatom.dim == 200
         assert skipatom.embedding_name == "skipatom"
         assert megnet16.dim == 16
         assert megnet16.embedding_name == "megnet16"
+        assert matscholar.dim == 200
+        assert matscholar.embedding_name == "matscholar"
         assert isinstance(skipatom.citation(), list)
         assert isinstance(megnet16.citation(), list)
+        assert isinstance(matscholar.citation(), list)
+        
     def test_Embedding_file_input(self):
         """Test that the Embedding class can load custom data."""
         embedding_csv = Embedding.from_csv(TEST_EMBEDDING_CSV)
