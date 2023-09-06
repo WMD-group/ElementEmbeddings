0492f872b654f23914897c8a08a1dec1583d2eaf
Anthony Onwuli <anthony.onwuli16@imperial.ac.uk>
2023-09-06 18:27:49 +0100

    Add config.py

---
+++ src/elementembeddings/utils/config.py
@@ -0,0 +1,14 @@
+"""Configuration variables for ElementEmbeddings."""
+DEFAULT_EMBEDDINGS = {
+    "magpie": "magpie.csv",
+    "magpie_sc": "magpie_sc.json",
+    "mat2vec": "mat2vec.csv",
+    "matscholar": "matscholar-embedding.json",
+    "megnet16": "megnet16.json",
+    "mod_petti": "mod_petti.json",
+    "oliynyk": "oliynyk_preprocessed.csv",
+    "oliynyk_sc": "oliynyk_sc.json",
+    "random_200": "random_200_new.csv",
+    "skipatom": "skipatom_20201009_induced.csv",
+    "atomic": "atomic.json",
+}
