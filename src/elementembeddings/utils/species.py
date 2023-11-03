3bac1d2f330438cdca075d9d7d4cd0f1311989a8
AntObi <aoo216@ic.ac.uk>
2023-11-03 16:27:07 +0000

    Add parse_species function

---
+++ src/elementembeddings/utils/species.py
@@ -0,0 +1,28 @@
+"""Utilities for species."""
+import re
+from typing import Tuple
+
+
+def parse_species(species: str) -> Tuple[str, int]:
+    """
+    Parse a species string into its atomic symbol and oxidation state.
+
+    :param species: the species string
+    :return: a tuple of the atomic symbol and oxidation state
+
+    """
+    ele = re.match(r"[A-Za-z]+", species).group(0)
+
+    charge_match = re.search(r"\d+", species)
+    ox_state = int(charge_match.group(0)) if charge_match else 0
+
+    if "-" in species:
+        ox_state *= -1
+
+    # Handle cases of X+ or X- (instead of X1+ or X1-)
+    if "+" in species and ox_state == 0:
+        ox_state = 1
+
+    if ox_state == 0 and "-" in species:
+        ox_state = -1
+    return ele, ox_state
