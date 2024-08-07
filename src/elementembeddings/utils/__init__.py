38779741c0627b3e15fa251ab39d3f7b2188b7f8
Anthony Onwuli <aoo216@ic.ac.uk>
2024-08-07 12:00:20 +0100

    Improve utils documentation

--- src/elementembeddings/utils/__init__.py
+++ src/elementembeddings/utils/__init__.py
@@ -1,1 +1,9 @@
-"""Utility functions for elementembeddings."""
+"""Utility functions for elementembeddings.
+
+Modules exported in `elementembeddings.utils`:
+
+- `elementembeddings.utils.config`: Provides configuration settings and constants.
+- `elementembeddings.utils.io`: Tools to read and write data.
+- `elementembeddings.utils.math`: Tools for mathematical operations.
+- `elementembeddings.utils.species`: Tools to handle ionic species.
+"""
